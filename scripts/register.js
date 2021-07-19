2e95f3d9e3e23ac8badb6cccfec4527e9c0ddfd8
Brett Smith <brett.a.smith1@outlook.com>
2021-07-18 21:34:35 -0700

    Week02 update

---
+++ scripts/register.js
@@ -0,0 +1,31 @@
+﻿//Check that user's password and 'confirm password' inputs match
+function validatePassword() {
+    if (document.getElementById("pass").value == document.getElementById("passConfirm").value) {
+        document.getElementById("passConfirm").className = "MssaValid";
+        document.getElementById("passConfirm").setCustomValidity("");
+        return true;
+    }
+    else {
+        //set background of passConfirm text block to pink (MssaInvalid class)
+        document.getElementById("passConfirm").className = "MssaInvalid";
+
+        //set custom validity error to display if passwords do not match
+        document.getElementById("passConfirm").setCustomValidity("Passwords do not match. Please check your password and try again.");
+        document.getElementById("passConfirm").reportValidity;
+        return false;
+    }
+        
+}
+
+//Check that user's password and 'confirm password' inputs match, display alert window if they do not
+function validateForm() {
+    if (document.getElementById("pass").value == document.getElementById("passConfirm").value) {
+        return true;
+    }
+    else {
+        //Alert window if passwords to not match on submit
+        window.alert("Passwords do not match")
+        return false;
+    }
+    
+}
